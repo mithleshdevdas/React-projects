88396bd93d597df467c77b9b5bb16560e3064aeb
mithlesh devdas <mithleshdevdas580@gmail.com>
2024-01-17 17:26:17 +0530

    counter project

---
+++ counter-app/src/counterapp/Counter.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+function Counter() {
+  // let counter = 5
+  let [counter, setcounter] = useState(0);
+  const AddValue = () => {
+    // console.log('add value', counter);
+    setcounter(counter + 1);
+  };
+  const removeValue = () => {
+    // console.log('remove value', counter);
+    setcounter(counter - 1);
+  };
+  return (
+    <>
+      <h1>counter value: {counter} </h1>
+      <button onClick={AddValue}>add value {counter}</button> <br /> <br />
+      <button onClick={removeValue}>remove value {counter}</button>
+    </>
+  );
+}
+
+export default Counter;
