d141b77c5d01b211950ec84fc0043bc3d20778b0
mithlesh devdas <mithleshdevdas580@gmail.com>
2024-01-17 17:25:56 +0530

    card js project

---
+++ counter-app/src/cards/Cards.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function Cards({username,btn}) {
+  return (
+      <>
+        
+          <h1>my name is {username}</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, a?</p>
+          <button> {btn}</button>
+         
+    </>
+  )
+}
+
+
+export default Cards
