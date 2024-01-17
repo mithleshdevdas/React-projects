4c8977dc88059e6af8b02c7721d3e64f987996f2
mithlesh devdas <mithleshdevdas580@gmail.com>
2024-01-17 17:25:36 +0530

    bgchanger project

---
+++ counter-app/src/bgchanger/Bgchanger.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+function Bgchanger() {
+    const [color,setcolor]= useState('green')
+    return (
+      <>
+        <div
+          style={{
+            background: color,
+            width: "100vw",
+                    height: "100vh",
+            padding: '5px'
+          }}
+        >
+          <div
+            style={{
+              border: "1px solid black",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              margin: "1rem",
+              padding: "1rem",
+                        borderRadius: "10px",
+                        background: 'pink',
+              width: '75vw'
+            }}
+          >
+            <button onClick={() => setcolor("black")}>black</button>
+            <button onClick={() => setcolor("orange")}>orange</button>
+            <button onClick={() => setcolor("purple")}>purple</button>
+            <button onClick={() => setcolor("olive")}>olive</button>
+            <button onClick={() => setcolor("red")}>red</button>
+            <button onClick={() => setcolor("blue")}>blue</button>
+          </div>
+        </div>
+      </>
+    );
+}
+
+export default Bgchanger
