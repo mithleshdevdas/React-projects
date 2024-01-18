80f50f0afe2eefac46e222dc35dc1140d5ff989a
mithlesh devdas <mithleshdevdas580@gmail.com>
2024-01-18 17:57:04 +0530

    use context api

---
+++ counter-app/src/context-api/ContextSetup.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Login from './Login'
+import Profile from './Profile'
+import UserContextProvider from './UserContextProvider'
+
+function ContextSetup() {
+  return (
+      <>
+          <UserContextProvider>
+              <Login />
+              <Profile/>
+      </UserContextProvider>
+    </>
+  )
+}
+
+export default ContextSetup
