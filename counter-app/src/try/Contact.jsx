79fe1ae6cf05f3e9cfdbcf4ee0238b06c637d887
mithlesh devdas <mithleshdevdas580@gmail.com>
2024-01-18 18:00:13 +0530

    router dom

---
+++ counter-app/src/try/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Passgen from './Passgen'
+
+function Contact() {
+  return (
+    <>
+      <div style={ { width: "100vw", height: "50vh" } }>
+        
+      <Passgen />
+      </div>
+    </>
+  );
+}
+
+export default Contact
