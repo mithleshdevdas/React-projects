ec7badc169f16f369fe6cd1154d75d4880de75ab
mithlesh devdas <mithleshdevdas580@gmail.com>
2024-01-18 17:59:27 +0530

    react router dom project

---
+++ counter-app/src/try/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Bgchager from './Bgchager'
+
+function About() {
+  return (
+    <>
+      <div style={ { width: "50vw", height: "50vh" } }>
+        
+      <Bgchager />
+      </div>
+    </>
+  );
+}
+
+export default About
