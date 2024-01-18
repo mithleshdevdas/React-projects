7e4b2ded9d4e9db0585a762f08ea80c840e14c0f
mithlesh devdas <mithleshdevdas580@gmail.com>
2024-01-18 18:00:54 +0530

    react-router-dom project

---
+++ counter-app/src/try/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Conter from './Conter'
+
+function Home() {
+  return (
+    <>
+      <div style={{width:'50vw',height:'50vh'}}>
+
+      <Conter/>
+      </div>
+  </>
+  )
+}
+
+export default Home
