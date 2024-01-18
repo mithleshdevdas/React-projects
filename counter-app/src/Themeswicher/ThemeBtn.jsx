6f52b3e467987d47f6a1da21c420433a4d80a5c3
mithlesh devdas <mithleshdevdas580@gmail.com>
2024-01-18 17:59:11 +0530

    theme project

---
+++ counter-app/src/Themeswicher/ThemeBtn.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import useTheme from "./theme";
+
+export default function ThemeBtn() {
+  const { themeMode, lightTheme, darkTheme } = useTheme();
+  const onChangeBtn = (e) => {
+    const darkModeStatus = e.currentTarget.checked;
+    if (darkModeStatus) {
+      darkTheme();
+    } else {
+      lightTheme();
+    }
+  };
+    return (
+         <>
+ 
+      
+          
+      <label>
+                <input
+                    type="checkbox"
+                    value=""
+                    checked={ themeMode === 'dark' }
+                    onChange={onChangeBtn}
+                /> <span>toggle thme</span>   
+      </label>
+      </>
+  );
+}
