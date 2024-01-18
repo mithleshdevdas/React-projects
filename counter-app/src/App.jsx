0ac1cb8b7f94e5e84f073f2a8fbf430ee8ee06bb
mithlesh devdas <mithleshdevdas580@gmail.com>
2024-01-18 17:56:51 +0530

    container change to every projects

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -5,37 +5,33 @@ import Cards from "./cards/Cards";
 import Bgchanger from "./bgchanger/Bgchanger";
 import PasswordGenter from "./passwordgenter/PasswordGenter";
 import Final from "./currencyconver/Final";
-import { Route, Router, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
-import Layout from "./React-Router-dom/Layout";
-import Home from "./React-Router-dom/Home";
-import About from "./React-Router-dom/About";
-import Contact from "./React-Router-dom/Contact";
-import Github from "./React-Router-dom/Github";
-import User from "./React-Router-dom/User";
+import Try from "./React-Router-dom/Try";
+
+import Bgchager from "./try/Bgchager";
+import Card from "./try/Card";
+import Conter from "./try/Conter";
+import Passgen from "./try/Passgen";
+import Layout from "./try/Layout";
+import UserContextProvider from "./context-api/UserContextProvider";
+import Login from "./context-api/Login";
+import Profile from "./context-api/Profile";
+import ContextSetup from "./context-api/ContextSetup";
+import Setup from "./Themeswicher/Setup";
 
 
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route path="" element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="contact" element={<Contact />} />
-      <Route path="github" element={<Github />} />
-      <Route path="user/:id" element={<User />} />
-    </Route>
-  )
-);
+
+
+
 
 
 function App() {
   return (
-    <>
- 
-<RouterProvider router={router} />
+ <>
+<Setup/>
+ </>
   
 
-</>
   )
 }
 
