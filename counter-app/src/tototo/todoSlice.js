455d27847d350b6e02b84a04eeb0f813c2f97c83
mithlesh devdas <mithleshdevdas580@gmail.com>
2024-01-30 14:36:03 +0530

    todo

---
+++ counter-app/src/tototo/todoSlice.js
@@ -0,0 +1,34 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit'
+
+const initialState ={
+    todos: [{
+        id: 1,
+        text: 'mith'
+    }]
+
+
+};
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addtodo: (state, action)=>{
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+            }
+            state.todos.push(todo)
+        },
+        removetodo: (state, action) => {
+            state.todos=state.todos.filter((todo)=> todo.id !== action.payload)
+        }
+
+        
+    }
+})
+
+export const { addtodo, removetodo } = todoSlice.actions
+
+
+export default todoSlice.reducer
