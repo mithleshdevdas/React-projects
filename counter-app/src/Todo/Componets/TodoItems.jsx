3c384c9e680d221dd0bde5f5a5173a67ea33e4be
mithlesh devdas <mithleshdevdas580@gmail.com>
2024-01-30 14:35:01 +0530

    todo

--- counter-app/src/Todo/Componets/TodoItems.jsx
+++ counter-app/src/Todo/Componets/TodoItems.jsx
@@ -61,7 +61,7 @@ function TodoItems(todo) {
           className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0"
           onClick={() => deleteTodo(todo.id)}
         >
-          ❌
+          ❌ 
         </button>
       </div>
     </>
