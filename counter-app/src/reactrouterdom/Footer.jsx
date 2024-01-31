dece1ff89c537b4809e0d772898b785ff9bc0d35
mithlesh devdas <mithleshdevdas580@gmail.com>
2024-01-31 15:14:28 +0530

    pracial

---
+++ counter-app/src/reactrouterdom/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Footer() {
+  return (
+    <>
+<footer className='bg-green-400'>
+        <ul>
+          <li>
+  <Link to="/">Home</Link>
+          </li>
+ <li>
+  <Link to="/about">About</Link>
+          </li>
+  <li>
+            <Link to="/contact">Contact</Link>
+ </li>
+        </ul>
+      </footer>
+    </>
+  );
+}
+
+export default Footer
