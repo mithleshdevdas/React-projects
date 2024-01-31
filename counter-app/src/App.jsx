21978edb73199f1f4ed2b3ddf116d7ffc977e547
mithlesh devdas <mithleshdevdas580@gmail.com>
2024-01-31 15:13:52 +0530

    pracial

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,52 +1,15 @@
-
-import React, { useState } from "react";
-import Counter from "./counterapp/Counter";
-import Cards from "./cards/Cards";
-import Bgchanger from "./bgchanger/Bgchanger";
-import PasswordGenter from "./passwordgenter/PasswordGenter";
-import Final from "./currencyconver/Final";
-import Try from "./React-Router-dom/Try";
-
-import Bgchager from "./try/Bgchager";
-import Card from "./try/Card";
-import Conter from "./try/Conter";
-import Passgen from "./try/Passgen";
-import Layout from "./try/Layout";
-import UserContextProvider from "./context-api/UserContextProvider";
-import Login from "./context-api/Login";
-import Profile from "./context-api/Profile";
-import ContextSetup from "./context-api/ContextSetup";
-import Setup from "./Themeswicher/Setup";
-import SetupTodo from "./Todo/SetupTodo";
-
-import AddTodo from './todoo/Addtodo';
-
-import { Provider } from "react-redux";
-
-import Todos from './todoo/Todos';
-
-import Todoss from "./tototo/Todoss";
-import Add from "./tototo/Add";
-import {store} from './tototo/store'
-
-
-
+import React from 'react'
+import Counter from './counter-project/Counter'
+import PasswordGentor from './pass-project/PasswordGentor'
+import Layout from './reactrouterdom/Layout'
+import Result from './reactrouterdom/Result'
 
 function App() {
   return (
- <Provider store={store}>
-      <Add/>
-      <Todoss/>
- </Provider>
-  
-
+    <>
+<Result/>
+    </>
   )
 }
 
 export default App
-
-
-
-
-
-
