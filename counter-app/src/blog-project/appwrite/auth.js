beac799b78165d7699b60c128cf8ab905c5e4977
mithlesh devdas <mithleshdevdas580@gmail.com>
2024-02-02 19:54:47 +0530

    mega blog project

---
+++ counter-app/src/blog-project/appwrite/auth.js
@@ -0,0 +1,74 @@
+import config from "../config/config";
+import { Account, Client, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectId);
+    this.account = new Account(this.client);
+    }
+    
+
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+
+      if (userAccount) {
+        // call another method
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+    
+    
+    
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      throw error;
+      console.log("Appwrite service:: getCurrentUer:: error", error);
+    }
+    return null;
+  }
+
+    
+    
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      throw error;
+      console.log("Appwrite service:: logout:: error", error);
+    }
+  }
+}
+
+
+
+const authService = new AuthService();
+
+export default authService;
