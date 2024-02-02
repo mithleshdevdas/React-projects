5dfb9b7e8923e44ef9a23ff3f4c01508ee1fc03b
mithlesh devdas <mithleshdevdas580@gmail.com>
2024-02-02 19:55:28 +0530

    mega blog project

---
+++ counter-app/src/blog-project/componets/header/LogoutBtn.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import authService from '../../appwrite/auth'
+import { logout } from '../../store/authSlice'
+
+function LogoutBtn() {
+    const dispatch = useDispatch()
+
+  const logoutHandler = () => {
+    authService.logout().then(() => {
+      dispatch(logout())
+    })
+  }
+
+  return (
+    <div>
+      <button onClick={logoutHandler}className='bg-orange-400'>logout</button>
+    </div>
+  );
+}
+
+export default LogoutBtn
