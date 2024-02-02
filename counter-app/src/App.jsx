4a70226046db3261d79d1aa94369e505b9550681
mithlesh devdas <mithleshdevdas580@gmail.com>
2024-02-02 19:54:37 +0530

    mega blog project

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,13 +1,40 @@
 // @ts-nocheck
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+
+import authService from './blog-project/appwrite/auth'
+import authSlice, { login, logout } from './blog-project/store/authSlice'
+import { Footer, Header } from './blog-project/componets'
+import {Outlet} from 'react-router-dom'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1>hello</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+        dispatch(login({userData}))
+        } else {
+          dispatch(logout())
+      }
+      })
+    .finally(()=> setLoading(false))
+
+  },[])
+
+  return !loading ? <div className='bg-green-300 min-h-screen '>
+    <div className='w-full block'>
+      <Header />
+      <main>
+ <Outlet/>
+        todo 
+      </main>
+      <Footer/>
+  </div>
+  </div> : (null)
 }
 
 export default App
