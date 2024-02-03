9e140f83b6e544637381c08d5cfbafb80bcfaf52
mithlesh devdas <mithleshdevdas580@gmail.com>
2024-02-03 18:40:31 +0530

    mega-project-contine

--- counter-app/src/blog-project/componets/index.js
+++ counter-app/src/blog-project/componets/index.js
@@ -2,5 +2,17 @@ import Header from "./header/Header";
 import Footer from "./footer/Footer";
 import Container from "./container/Container";
 import LogoutBtn from "./header/LogoutBtn";
+import Button from './Button'
+import Input from "./Input";
+import Select from "./Select";
+import RTE from "./RTE";
+import SignUp from "./SignUp";
+import Login from "./Login";
+import PostCard from './PostCard'
+import PostForm from './post-form/PostForm'
 
-export{ Header, Footer,Container,LogoutBtn }
+import AuthLayout from './AuthLayout'
+
+export {
+    Header, Footer, Container, LogoutBtn, Button, Input, Select, RTE,
+SignUp,Login, PostCard,PostForm,AuthLayout}
