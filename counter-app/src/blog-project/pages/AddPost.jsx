e9b039b1924bdc8f6f71e8668889c29261c66bd4
mithlesh devdas <mithleshdevdas580@gmail.com>
2024-02-03 18:41:04 +0530

    mega-project-contine

---
+++ counter-app/src/blog-project/pages/AddPost.jsx
@@ -0,0 +1,17 @@
+// @ts-nocheck
+import React from 'react'
+
+import { Container, PostForm } from "../componets";
+
+
+function AddPost() {
+  return (
+      <div className='py-8'>
+          <Container>
+              <PostForm/>
+          </Container>
+ </div>
+  )
+}
+
+export default AddPost
