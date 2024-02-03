2e553c934294a89511766705240b038519b2de99
mithlesh devdas <mithleshdevdas580@gmail.com>
2024-02-03 18:41:25 +0530

    mega-project-contine

---
+++ counter-app/src/blog-project/pages/Login.jsx
@@ -0,0 +1,13 @@
+// @ts-nocheck
+import React from 'react'
+import { Login as loginComponet } from '../componets'
+
+function Login() {
+    return (
+        <div className="py-8">
+      <loginComponet/>
+  </div>
+    )
+}
+
+export default Login
