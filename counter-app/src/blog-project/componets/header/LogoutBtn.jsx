4071d2acabbcf85ef5ec682300f2149cdb8fa983
mithlesh devdas <mithleshdevdas580@gmail.com>
2024-02-04 11:08:40 +0530

    mega-project

--- counter-app/src/blog-project/componets/header/LogoutBtn.jsx
+++ counter-app/src/blog-project/componets/header/LogoutBtn.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import authService from '../../appwrite/auth'
-import { logout } from '../../store/authSlice'
+import React from "react";
+import { useDispatch } from "react-redux";
+import authService from "../../appwrite/auth";
+import  {logout } from "../../store/authSlice";
 
 function LogoutBtn() {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const logoutHandler = () => {
     authService.logout().then(() => {
-      dispatch(logout())
-    })
-  }
+      dispatch(logout());
+    });
+  };
 
   return (
     <div>
-      <button onClick={logoutHandler}className='bg-orange-400'>logout</button>
+      <button onClick={logoutHandler} className="bg-orange-400">
+        logout
+      </button>
     </div>
   );
 }
 
-export default LogoutBtn
+export default LogoutBtn;
--- counter-app/src/blog-project/componets/index.js
+++ counter-app/src/blog-project/componets/index.js
@@ -2,17 +2,29 @@ import Header from "./header/Header";
 import Footer from "./footer/Footer";
 import Container from "./container/Container";
 import LogoutBtn from "./header/LogoutBtn";
-import Button from './Button'
+import Button from "./Button";
 import Input from "./Input";
 import Select from "./Select";
 import RTE from "./RTE";
 import SignUp from "./SignUp";
 import Login from "./Login";
-import PostCard from './PostCard'
-import PostForm from './post-form/PostForm'
+import PostCard from "./PostCard";
+import PostForm from "./post-form/PostForm";
 
-import AuthLayout from './AuthLayout'
+import AuthLayout from "./AuthLayout";
 
 export {
-    Header, Footer, Container, LogoutBtn, Button, Input, Select, RTE,
-SignUp,Login, PostCard,PostForm,AuthLayout}
+  Header,
+  Footer,
+  Container,
+  LogoutBtn,
+  Button,
+  Input,
+  Select,
+  RTE,
+  SignUp,
+  Login,
+  PostCard,
+  PostForm,
+  AuthLayout,
+};
