71e59a8515bb099787d456b9b208472becdd9aff
mithlesh devdas <mithleshdevdas580@gmail.com>
2024-02-09 12:16:24 +0530

    mega

---
+++ counter-app/src/context/Login.jsx
@@ -0,0 +1,43 @@
+import React, { useContext, useState } from "react";
+import UserContext from "./UserContext";
+
+function Login() {
+  const [username, setusername] = useState("");
+  const [password, setpassword] = useState("");
+
+  const { setuser } = useContext(UserContext);
+
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    setuser({ username, password });
+  };
+  return (
+    <>
+      <h2>login</h2>
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(e) => {
+          setusername(e.target.value);
+        }}
+      />{" "}
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => {
+          setpassword(e.target.value);
+        }}
+      />{" "}
+      <button onClick={handlesubmit}>submit</button>
+    </>
+  );
+}
+
+export default Login;
+
+//
+//
+
+//
