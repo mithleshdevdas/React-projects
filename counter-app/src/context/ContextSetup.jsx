ab403c5e058b72ba93518c94de0413879505cfaa
mithlesh devdas <mithleshdevdas580@gmail.com>
2024-02-09 12:16:20 +0530

    mega

---
+++ counter-app/src/context/ContextSetup.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import Login from "./Login";
+import Profile from "./Profile";
+import UserContextProvider from "./UserContextProvider";
+
+function ContextSetup() {
+  return (
+    <>
+      <UserContextProvider>
+        <Login />
+        <Profile />
+      </UserContextProvider>
+    </>
+  );
+}
+
+export default ContextSetup;
+
+//
+//
+//
