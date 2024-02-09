2c36158e3a3ed6825f0ffb81fe2356a134df6ca7
mithlesh devdas <mithleshdevdas580@gmail.com>
2024-02-09 12:16:50 +0530

    mega

--- counter-app/src/counter-project/Counter.jsx
+++ counter-app/src/counter-project/Counter.jsx
@@ -1,38 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function Counter() {
-    const [counter, setcounter] = useState(0)
-    
-    const add = () => {
-        if (counter == counter) {
-            
-            setcounter(counter +1) 
-        } if (counter === 20) {
-            setcounter(counter)
-        }
+  const [counter, setcounter] = useState(0);
+
+  const add = () => {
+    if (counter == counter) {
+      setcounter(counter + 1);
     }
-    const remove = () => {
-        if (counter > 0) {
-            setcounter(counter -1)
-        }
+    if (counter == 20) {
+      setcounter(counter);
     }
+  };
+
+  const remove = () => {
+    if (counter > 0) {
+      setcounter(counter - 1);
+    }
+  };
   return (
     <>
-      <h1>
-        counter app{" "}
-        <span >
-          <b>{counter}</b>
-        </span>
-          </h1>
-          <div className="btn"style={{display: 'flex', justifyContent: 'space-around'}}>
-              
-      <button onClick={add}>
-        <b>add</b>
-          </button>
-          <button onClick={remove}><b>remove</b></button>
-          </div>
+      <div>
+        <h1>
+          {" "}
+          couter app{" "}
+          <span>
+            <b>{counter}</b>
+          </span>
+        </h1>
+        <button onClick={add}>add</button>
+        <button onClick={remove}>remove</button>
+      </div>
     </>
   );
 }
 
-export default Counter
+export default Counter;
