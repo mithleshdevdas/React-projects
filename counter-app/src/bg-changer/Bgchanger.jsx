b4a487f58217424fd00ad946b23bdcfebd048164
mithlesh devdas <mithleshdevdas580@gmail.com>
2024-02-12 13:45:01 +0530

    mega

--- counter-app/src/bg-changer/Bgchanger.jsx
+++ counter-app/src/bg-changer/Bgchanger.jsx
@@ -1,23 +1,19 @@
 import React, { useState } from 'react'
 
 function Bgchanger() {
-    const [color,setcolor]= useState('gray')
+  const [color, setcolor] = useState('olive')
   return (
     <>
-      <div style={{ background: color, height: "100vh" }}>
-        <h1>bg-changer-app</h1>
-        <div>
-          <button onClick={() => setcolor("red")}>red</button>
-          <button onClick={() => setcolor("yellow")}>yellow</button>
-          <button onClick={() => setcolor("green")}>green</button>
-          <button onClick={() => setcolor("purple")}>purple</button>
-          <button onClick={() => setcolor("orange")}>orange</button>
-          <button onClick={() => setcolor("pink")}>pink</button>
-          <button onClick={() => setcolor("olive")}>olive</button>
-        </div>
+      <div  style={{height: '100vh', background: color }}>
+        <h2>bg changer app</h2>
+
+        <button onClick={()=>setcolor('red')}>red</button>
+        <button onClick={()=>setcolor('green')}>green</button>
+        <button onClick={()=>setcolor('blue')}>blue</button>
+        
       </div>
     </>
-  );
+  )
 }
 
 export default Bgchanger
