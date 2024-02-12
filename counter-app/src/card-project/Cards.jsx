2f6d2e27e4273413e93d3bd488d113477c05dbb6
mithlesh devdas <mithleshdevdas580@gmail.com>
2024-02-12 13:45:05 +0530

    mega

--- counter-app/src/card-project/Cards.jsx
+++ counter-app/src/card-project/Cards.jsx
@@ -3,13 +3,10 @@ import React from 'react'
 function Cards({name,btn}) {
   return (
     <>
-      <div style={{height: '200px', border: '1px solid', width: '350px', margin: '0 auto',textAlign: 'center'}}>
-        <h2>my nme is <b>{ name}</b></h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate eum vitae aliquid.</p>
-        <button><b>{ btn}</b></button>
-</div>
-
-    </>
+      <h2>my name is <b>{ name }</b></h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, distinctio?</p>
+      <button><b>{ btn}</b></button>
+</>
   )
 }
 
