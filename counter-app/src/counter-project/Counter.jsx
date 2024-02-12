6dae710d53fb53cb312d618584fb7524d2f93a75
mithlesh devdas <mithleshdevdas580@gmail.com>
2024-02-12 13:45:32 +0530

    mega

--- counter-app/src/counter-project/Counter.jsx
+++ counter-app/src/counter-project/Counter.jsx
@@ -1,37 +1,30 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 function Counter() {
-  const [counter, setcounter] = useState(0);
-
+  const [counter, setconter] = useState(0)
+  
   const add = () => {
     if (counter == counter) {
-      setcounter(counter + 1);
+      setconter(counter +1)
+    } if (counter == 20) {
+      setconter(counter )
     }
-    if (counter == 20) {
-      setcounter(counter);
-    }
-  };
-
+  }
+  
+  
   const remove = () => {
     if (counter > 0) {
-      setcounter(counter - 1);
+      setconter(counter -1)
     }
-  };
+  }
   return (
     <>
-      <div>
-        <h1>
-          {" "}
-          couter app{" "}
-          <span>
-            <b>{counter}</b>
-          </span>
-        </h1>
-        <button onClick={add}>add</button>
-        <button onClick={remove}>remove</button>
-      </div>
-    </>
-  );
+      <h3>counter app <span> <b>{ counter }</b></span></h3>
+      
+      <button onClick={add}>add</button> {""}
+      <button onClick={remove}>remove</button>
+ </>
+  )
 }
 
-export default Counter;
+export default Counter
